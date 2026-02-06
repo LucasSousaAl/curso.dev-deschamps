742f3c62d713e3f180865b11a0c29281546eb85a
Lucas Sousa <lucas.sousa277@hotmail.com>
2026-02-05 22:52:50 -0300

    adicionando variaveis de ambiente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(req, res) {
+import database from '../../../../infra/database';
+
+async function status(req, res) {
+  console.log(await database.query("SELECT 1 + 1 as sum;"));
   res.status(200).json({ status: 'ok' });
   // res.status(200).send("lau");
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
 test('GET /api/v1/status returns 200', async () => {
-  const response = await fetch('/api/v1/status');
+  const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
 });
